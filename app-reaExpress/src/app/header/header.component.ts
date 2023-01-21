eb3b8116aa8ba9dac4aae73d314e0315b30e32a9
Sampana5 <songa.sampana_yannick@courrier.uqam.ca>
2023-01-21 16:44:11 +0100

    latest version

--- app-reaExpress/src/app/header/header.component.ts
+++ app-reaExpress/src/app/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+
+
+declare function Mytest(): void;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+
+
+export class HeaderComponent implements OnInit{
+
+  myscriptElement: HTMLScriptElement | undefined;
+
+  ngOnInit(): void {
+    // let menubar = document.querySelector("#menubar");
+    // let mynav = document.querySelector(".navbar");
+
+    // menubar.
+  }
+
+  constructor(){
+    this.myscriptElement = document.createElement("script");
+    this.myscriptElement.src = "src/assets/js/custom.js";
+  }
+
+  CallExternalJsFunction(){
+    Mytest();
+  }
 
 }
+
+
---
+++ app-reaExpress/src/assets/js/custom.js
@@ -0,0 +1,24 @@
+// alert("pop up");
+function Mytest(){
+  let menubar = document.querySelector('#menubar');
+  let mynav = document.querySelector('.navbar');
+  // document.querySelector("#menubar").ClassList.toggle('fa-times')
+  menubar.classList.toggle('fa-times');
+  mynav.classList.toggle('active');
+
+  // x.classList.toggle("fa-thumbs-down")
+  // document.querySelector("#menubar").style.background = 'red';
+
+}
+
+
+
+//  let menubar = document.querySelector('#menubar');
+//   let mynav = document.querySelector('.navbar');
+
+//   menubar.onclick = () =>{
+//     menubar.classList.toggle('fa times');
+//   }
+
+  // document.querySelector('.fa fa-bars"').classList.toggle('fa times');
+
